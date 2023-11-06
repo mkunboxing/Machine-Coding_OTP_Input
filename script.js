0a1fb05b8250e98b2727c02b2c98f209e348330d
Mukul Kumar <mkunboxing@gmail.com>
2023-11-06 21:29:01 +0530

    fixed some bugs

--- script.js
+++ script.js
@@ -3,28 +3,38 @@ console.log(inputs)
 
 //event bubbling
 //move focus to next element sibling
-inputs.addEventListener('input',function(e){
-    let node=e.target
-    if (isNaN(node.value)==false){
-        let target = e.target.nextElementSibling
-        if (target){
-            target.focus()
-            }
-    }
-    else{
-        node.value=""
-        return
-    }
-})
+// inputs.addEventListener('input',function(e){
+//     let node=e.target
+//     if (isNaN(node.value)==false){
+//         let target = e.target.nextElementSibling
+//         if (target){
+//             target.focus()
+//             }
+//     }
+//     else{
+//         node.value=""
+//         return
+//     }
+// })
 
 //move focus to previous element sibling
 inputs.addEventListener('keyup',function(e){
+    // e.preventDefault();
+    let target;
     if(e.key=='Backspace'||e.key=='Delete'){
+        
         e.target.value =""
-        let target=e.target.previousElementSibling
-        if(target){
-            target.focus()
-        }
+     target=e.target.previousElementSibling
+        
+    }
+
+    if(e.key <= 9 && e.key >= 0){
+        e.target.value = e.key;
+     target=e.target.nextElementSibling
+    }
+    if(target){
+        target.focus();
     }
 })
+
 
